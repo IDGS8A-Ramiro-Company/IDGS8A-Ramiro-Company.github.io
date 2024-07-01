a3786e36d0b2db92f0e8b0d1ef73f734de0fd4e4
Angel Vargas <angel.delacruz@janusautomation.com>
2024-06-30 20:18:47 -0600

    Definicion de service worker

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'app-shell-cache';
+const urlsToCache = [
+    '/',
+    '/styles.css',
+    '/app.js',
+    '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
